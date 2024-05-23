8e7e93471fee15762d86905e9739dbc032aea561
78KLM <kalembaexoce@gmail.com>
2024-05-23 10:43:01 +0200

    pepe3

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    /**
+     * @Route("/contacts", name="app_contacts")
+     */
+    public function listeContact(): Response
+    {
+        return $this->render('contact/listeContacts.html.twig');
+    }
+}
